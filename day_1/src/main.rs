a6894ee3be6f552120c0f824577d676d301e5f5d
Peter <peterr.00000@gmail.com>
2022-12-01 14:53:14 +0800

    day 1 part 1 & 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+const PATH: &str = "src/input";
+
 fn main() {
-    println!("Hello, world!");
+    let data = fs::read_to_string(PATH).expect("Error reading file");
+    let mut cals_list: Vec<u32> = Vec::new();
+    let mut cals: u32 = 0;
+    for line in data.split("\n") {
+        if line.len() == 0 {
+            cals_list.push(cals);
+            cals = 0;
+        } else {
+            cals += line.parse::<u32>().unwrap();
+        }
+    }
+    cals_list.sort_by(|a, b| b.cmp(a));
+    println!("PART 1: {}", cals_list[0]);
+    println!("PART 2: {}", cals_list[0] + cals_list[1] + cals_list[2]);
 }
